d8c2d3cf6126eda155ec2da4e45f407dd7043996
rich@TIA <rich@skillseeker.io>
2023-09-13 12:13:12 +1000

    Add cognito pool to config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_9S9wyHDhf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7set40siac7ls6g7ge99jmfj2c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
